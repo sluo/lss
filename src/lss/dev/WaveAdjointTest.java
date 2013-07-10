a21055152c0d7c9bdffef1dcbe9c5a46aa24c9a1
Simon Luo <sluo@mines.edu>
2013-07-09 18:34:41 -0600

    sync

---
+++ src/lss/dev/WaveAdjointTest.java
@@ -0,0 +1,123 @@
+package lss.dev;
+
+import edu.mines.jtk.dsp.*;
+import edu.mines.jtk.mosaic.*;
+import static edu.mines.jtk.util.ArrayMath.*;
+
+public class WaveAdjointTest {
+
+  public WaveAdjointTest(
+    Sampling sz, Sampling sx, Sampling st)
+  {
+    _dx = sx.getDelta();
+    _dt = st.getDelta();
+    _nx = sx.getCount();
+    _nt = st.getCount();
+  }
+
+  public static void propagateForward(float[] a, float[][] u) {
+    int nx = u[0].length;
+    int nt = u.length;
+    float[] um,ui,up;
+    float[] ut = new float[nx];
+    for (int it=1; it<nt-1; ++it) {
+      um = u[it-1];
+      ui = u[it  ];
+      up = u[it+1];
+      applyForward(a,ui,ut);
+      sub(ut,um,up);
+    }
+  }
+
+  private static float[] applyForward(float[] a, float[] x) {
+    float[] y = new float[x.length];
+    applyForward(a,x,y);
+    return y;
+  }
+  private static void applyForward(float[] a, float[] x, float[] y) {
+    int n = x.length;
+    float ai;
+    ai = a[0];
+    y[0] = 2.0f*(1.0f-ai)*x[0]+ai*x[1];
+    for (int i=1; i<n-1; ++i) {
+      ai = a[i];
+      y[i] = ai*(x[i-1]+x[i+1])+2.0f*(1.0f-ai)*x[i];
+    }
+    ai = a[n-1];
+    y[n-1] = 2.0f*(1.0f-ai)*x[n-1]+ai*x[n-2];
+  }
+
+  private static float[] applyAdjoint(float[] a, float[] x) {
+    float[] y = new float[x.length];
+    applyAdjoint(a,x,y);
+    return y;
+  }
+  private static void applyAdjoint(float[] a, float[] x, float[] y) {
+    int n = x.length;
+    float am,ai,ap;
+    ai = a[0];
+    ap = a[1];
+    y[0] = 2.0f*(1.0f-ai)*x[0]+ap*x[1];
+    for (int i=1; i<n-1; ++i) {
+      am = a[i-1];
+      ai = a[i  ];
+      ap = a[i+1];
+      y[i] = am*x[i-1]+2.0f*(1.0f-ai)*x[i]+ap*x[i+1];
+    }
+    am = a[n-2];
+    ai = a[n-1];
+    y[n-1] = am*x[n-2]+2.0f*(1.0f-ai)*x[n-1];
+  }
+
+  //////////////////////////////////////////////////////////////////////////
+  // private
+
+  private int _it; // current time index
+  private int _nt;
+  private int _nx;
+  private double _dt;
+  private double _dx;
+
+  //////////////////////////////////////////////////////////////////////////
+  // testing
+  
+  public static void main(String[] args) {
+    //adjointTest();
+    propagationTest();
+  }
+
+  private static void propagationTest() {
+    Sampling sx = new Sampling(501,0.0120,0.0);
+    Sampling st = new Sampling(2001,0.0015,0.0);
+    int nx = sx.getCount();
+    int nt = st.getCount();
+    double dx = sx.getDelta();
+    double dt = st.getDelta();
+    float[] s = fillfloat(0.25f,nx); // slowness
+    float[] a = div((float)(dt*dt/(dx*dx)),mul(s,s));
+    float[][] u = new float[nt][nx];
+    u[0][nx/2] = 1.0f;
+    //new RecursiveGaussianFilter(0.050/0.012).apply0(u[0],u[0]);
+    new RecursiveGaussianFilter(0.100/0.012).apply20(u,u);
+    SimplePlot.asPoints(u[0]);
+    propagateForward(a,u);
+    SimplePlot.asPixels(u);
+  }
+
+  private static void adjointTest() {
+    int n = 10;
+    float[] x = randfloat(n);
+    float[] y = randfloat(n);
+    float[] s = randfloat(n);
+    System.out.println(dot(applyForward(s,x),y));
+    System.out.println(dot(applyAdjoint(s,y),x));
+  }
+
+  private static float dot(float[] x, float[] y) {
+    int n = x.length;
+    float sum = 0.0f;
+    for (int i=0; i<n; ++i)
+      sum += x[i]*y[i];
+    return sum;
+  }
+}
--- src/lss/vel/objective.py
+++ src/lss/vel/objective.py
@@ -5,13 +5,17 @@ from imports import *
 
 #############################################################################
 
+# Time sampling
 dt = 0.01*PI
-nt = 1+int(40.0*PI/dt)
+nt = 1+int(30.0*PI/dt)
 ft = -0.5*nt*dt
 st = Sampling(nt,dt,ft)
 
 pngdatDir = None
-#pngdatDir = '/Users/sluo/Desktop/pngdat/'
+pngdatDir = '/Users/sluo/Desktop/pngdat/'
+
+#widthPoints = None # slides
+widthPoints = 223.0 # single column
 
 #############################################################################
 
@@ -21,6 +25,21 @@ def main(args):
   showGlobalMinimum = False
   showLocalMinimum = False
 
+  # Time lag sampling
+  #nl = nt/4 # number of time lags
+  nl = 650
+  fl = -nl/2 # first lag
+  dl = dt
+  sl = Sampling(nl,dl,fl*dl)
+
+  # Amplitude scale sampling
+  fa = 0.0
+  la = 2.0
+  da = 0.05
+  na = 1+int((la-fa)/da)
+  sa = Sampling(na,da,fa)
+
+  # Simulate data
   ds = zerofloat(nt)
   for it in range(nt):
     t = ft+it*dt
@@ -30,25 +49,18 @@ def main(args):
   mul(w,ds,ds)
   div(ds,max(abs(ds)),ds)
 
+  # Observed data
+  amplitudeScale = 1.5
+  timeShift = 1.5*PI
   if (not useAmplitudeResidual) and (not showGlobalMinimum):
-    do = shift(ds,1.5*PI)
+    do = shift(ds,timeShift)
   else:
     do = copy(ds)
-  mul(1.5,do,do) # scale
+  mul(amplitudeScale,do,do) # scale
 
-  nl = nt/4 # number of time lags
-  fl = -nl/2 # first lag
-  dl = dt
-  sl = Sampling(nl,dl,fl*dl)
-
-  fa = 0.0
-  la = 2.0
-  da = 0.05
-  na = 1+int((la-fa)/da)
-  sa = Sampling(na,da,fa)
-
-  corr = Correlation(Correlation.Measure.ABSOLUTE_DIFFERENCE)
-  #corr = Correlation(Correlation.Measure.SQUARED_DIFFERENCE)
+  # Objective function
+  #corr = Correlation(Correlation.Measure.ABSOLUTE_DIFFERENCE)
+  corr = Correlation(Correlation.Measure.SQUARED_DIFFERENCE)
   def makeObjectiveFunction(ds,do):
     ob = zerofloat(nl,na)
     class Loop(Parallel.LoopInt):
@@ -67,15 +79,15 @@ def main(args):
 
   ob = makeObjectiveFunction(ds,do)
   #plot(ob,sl,sa,,cmin=0.0,cmax=1.0,title='obj')
-  plot(ob,Sampling(nl,0.1*dl,0.1*fl*dl),sa,cmin=0.0,cmax=1.0,title='obj')
+  plot(ob,Sampling(nl,0.1*dl,0.1*fl*dl),sa,jet,cmin=0.0,cmax=1.0,title='obj')
 
   if showGlobalMinimum:
     mul(1.5,ds,ds)
   if showLocalMinimum:
     ds = shift(ds,-0.5*PI)
-    points(st,ds,do,title='ds_do')
+    #points(st,ds,do,title='ds_do')
+    #plot(obj,sl,sa,cmin=0.0,cmax=1.0,title='obj')
     obj = makeObjectiveFunction(ds,do)
-    plot(obj,sl,sa,cmin=0.0,cmax=1.0,title='aaa')
     lmin = 1.0e6
     aopt = 0.0
     for ia in range(na):
@@ -89,10 +101,14 @@ def main(args):
   #points(st,ds,title='ds')
   #points(st,do,title='do')
   #points(st,ds,do,title='ds_do')
-  cmax = 1.1*max(max(ds),max(do)); cmin = -cmax
+  #cmax = 1.25*max(do); cmin = -cmax
+  cmax = la; cmin = -cmax
   points(Sampling(nt,0.1*dt,0.1*ft),ds,cmin=cmin,cmax=cmax,title='ds')
   points(Sampling(nt,0.1*dt,0.1*ft),do,cmin=cmin,cmax=cmax,title='do')
-  points(Sampling(nt,0.1*dt,0.1*ft),ds,do,cmin=cmin,cmax=cmax,title='ds_do')
+  #points(Sampling(nt,0.1*dt,0.1*ft),ds,do,cmin=cmin,cmax=cmax,title='ds_do')
+  points(Sampling(nt,0.1*dt,0.1*ft),do,ds,cmin=cmin,cmax=cmax,title='do_ds')
+  points(Sampling(nt,0.1*dt,0.1*ft),do,ds,shift(do,-timeShift),
+    cmin=cmin,cmax=cmax,title='do_ds_shift')
 
 def shift(f,tau):
   g = like(f)
@@ -122,15 +138,19 @@ def plot(f,s1=None,s2=None,cmap=jet,
   panel = PlotPanel()
   panel.setHLimits(s1.first,s1.last)
   panel.setVLimits(s2.first,s2.last)
-  panel.setHLabel('Time shift')
+  panel.setHLabel('Time shift (s)')
   panel.setVLabel('Amplitude scale')
   cb = panel.addColorBar()
-  cb.setLabel('Error')
-  cb.setWidthMinimum(160)
+  cb.setLabel('Normalized error')
+  if widthPoints is None:
+    cb.setWidthMinimum(160)
+  elif widthPoints==223.0:
+    cb.setWidthMinimum(140)
   if s1 is not None and s2 is not None:
     pixel = panel.addPixels(s1,s2,f)
-    #contour = panel.addContours(s1,s2,f)
-    #contour.setLineColor(Color.BLACK)
+    contour = panel.addContours(s1,s2,f)
+    contour.setLineColor(Color.BLACK)
+    contour.setLineWidth(1.0)
   else:
     pixel = panel.addPixels(f)
   pixel.setColorModel(cmap)
@@ -144,46 +164,69 @@ def plot(f,s1=None,s2=None,cmap=jet,
     pixel.setClips(-clip,clip)
   pixel.setInterpolation(PixelsView.Interpolation.LINEAR)
   frame = PlotFrame(panel)
-  frame.setFontSizeForSlide(1.0,1.0)
-  frame.setSize(1200,500)
+  if widthPoints is None:
+    frame.setFontSizeForSlide(1.0,1.0)
+    frame.setSize(1200,500)
+  elif widthPoints==223.0:
+    frame.setFontSizeForPrint(8,widthPoints)
+    frame.setSize(1200,500)
   if title:
     frame.setTitle(title)
   frame.setVisible(True)
   if title and pngdatDir:
-    #frame.paintToPng(360,3.0,pngdatDir+title+'.png')
-    frame.paintToPng(720,3.08,pngdatDir+title+'.png')
-    #write(pngdatDir+title+'.dat',f)
+    if widthPoints is None:
+      #frame.paintToPng(360,3.0,pngdatDir+title+'.png')
+      frame.paintToPng(720,3.08,pngdatDir+title+'.png')
+      #write(pngdatDir+title+'.dat',f)
+    elif widthPoints==223.0:
+      frame.paintToPng(360,widthPoints/72.0,pngdatDir+title+'.png')
   return panel
 
-def points(s,x1,x2=None,cmin=0.0,cmax=0.0,title=None):
+def points(s,x1,x2=None,x3=None,cmin=0.0,cmax=0.0,title=None):
   panel = PlotPanel()
   panel.setHLabel('Time (s)')
   panel.setVLabel('Amplitude')
   panel.setHInterval(2.0)
+  if widthPoints is None:
+    lineWidth = 3.0
+  elif widthPoints==223.0:
+    lineWidth = 4.0
   point1 = panel.addPoints(s,x1)
   point1.setLineColor(Color.BLACK)
-  point1.setLineWidth(3.0)
-  max1 = 1.1*max(abs(x1))
-  panel.setVLimits(-max1,max1)
+  point1.setLineWidth(lineWidth)
+  #max1 = 1.1*max(abs(x1))
+  #panel.setVLimits(-max1,max1)
   if x2 is not None:
-    max2 = 1.1*max(abs(x2))
+    #max2 = 1.1*max(abs(x2))
     point2 = panel.addPoints(s,x2)
     point2.setLineColor(Color.RED)
-    #point2.setLineStyle(PointsView.Line.DOT)
-    point2.setLineWidth(3.0)
-    panel.setVLimits(-max(max1,max2),max(max1,max2))
+    #point2.setLineStyle(PointsView.Line.DASH)
+    point2.setLineWidth(lineWidth)
+    #panel.setVLimits(-max(max1,max2),max(max1,max2))
+  if x3 is not None:
+    point3 = panel.addPoints(s,x3)
+    point3.setLineColor(Color.BLUE)
+    #point3.setLineStyle(PointsView.Line.DOT)
+    point3.setLineWidth(lineWidth)
   if cmin<cmax:
     panel.setVLimits(cmin,cmax)
   frame = PlotFrame(panel)
-  frame.setFontSizeForSlide(0.86,0.86)
-  frame.setSize(1200,500)
+  if widthPoints is None:
+    frame.setFontSizeForSlide(0.86,0.86)
+    frame.setSize(1200,500)
+  elif widthPoints==223.0:
+    frame.setFontSizeForPrint(8,widthPoints)
+    frame.setSize(1200,500)
   if title:
     frame.setTitle(title)
   frame.setVisible(True)
   if title and pngdatDir:
-    #frame.paintToPng(360,3.0,pngdatDir+title+'.png')
-    frame.paintToPng(720,3.08,pngdatDir+title+'.png')
-    #write(pngdatDir+title+'.dat',f)
+    if widthPoints is None:
+      #frame.paintToPng(360,3.0,pngdatDir+title+'.png')
+      frame.paintToPng(720,3.08,pngdatDir+title+'.png')
+      #write(pngdatDir+title+'.dat',f)
+    elif widthPoints==223.0:
+      frame.paintToPng(720,widthPoints/72.0,pngdatDir+title+'.png')
 
 def read(name,image=None):
   if not image:
