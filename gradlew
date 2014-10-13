a0851a585dc06ff477c1e7799191e0ec162fa430
Esteban Díaz <ediazpan@mines.edu>
2014-10-13 10:37:13 -0600

    Source and Receiver class changes
    Now, it supports non integer coordinates injection, using sinc
    interpolation

---
+++ gradlew
@@ -0,0 +1,1 @@
+../jtk/gradlew
--- src/lss/mod/Source.java
+++ src/lss/mod/Source.java
@@ -2,6 +2,8 @@ package lss.mod;
 
 import edu.mines.jtk.util.Parallel;
 import static edu.mines.jtk.util.ArrayMath.*;
+import edu.mines.jtk.dsp.SincInterpolator;
+import edu.mines.jtk.dsp.Sampling;
 
 public interface Source {
 
@@ -79,6 +81,89 @@ public interface Source {
     private int _xs,_zs,_nt;
   }
 
+
+
+  /* this wavelet source injects the source in a 2D sinc
+     window around the true location (which may not lie on 
+     a grid point
+     Graham. J. Hicks (2002). 
+     ”Arbitrary source and receiver positioning in finite‐difference schemes 
+     using Kaiser windowed sinc functions.” 67, SPECIAL
+     SECTION—SEISMIC SIGNATURES OF FLUID TRANSPORT, 156-165.
+     doi: 10.1190/1.1451454
+  */
+  public static class WaveletSourceSinc implements Source {
+    public WaveletSourceSinc(float xs, float zs, float[] w) {
+      _xs = xs;
+      _ixs = (int) (xs+0.5f); // round to nearest grid point
+      _zs = zs; 
+      _izs = (int) (zs+0.5f); // round to nearest grid point
+      _nt = w.length;
+      _w = w;
+      buildTable();
+    }
+
+
+    public void add(float[][] ui, int it, int nabsorb) {
+      if (it>=_nt) 
+        return;
+
+      int n = (int) _n/2;
+
+      /* Scatter wavelet in the surounding locations */
+      for (int i2=0; i2<_n; i2++){
+        int i2u = -n +i2;        
+        for(int i1=0; i1<_n; i1++){
+          int i1u = -n +i1;
+          ui[_izs+nabsorb+i2u][_ixs+nabsorb+i1u] += _sinc_weights[i2][i1]*_w[it];
+        }
+      }
+    }
+
+    private void buildTable(){
+      _n = 9;
+      float o =(int) (-_n/2)+1;
+      SincInterpolator sinc = new SincInterpolator();
+
+      /* Here I build the input spike */
+      Sampling is1 = new Sampling(_n,1,o);
+      Sampling is2 = is1;
+      float[][] ispike = new float[_n][_n];
+      ispike[(int)_n/2][(int)_n/2] = 1.0f;
+
+
+      /* Here I build the shifted spike */
+      float d1 = (float) _xs - _ixs;
+      float d2 = (float) _zs - _izs; 
+
+      Sampling os1 = new Sampling(_n,1,o+d1);
+      Sampling os2 = new Sampling(_n,1,o+d2);
+
+      _sinc_weights = new float[_n][_n];
+
+      for (int i2=0; i2< os2.getCount(); i2++){
+        float z = (float) os2.getValue(i2);
+        for (int i1=0; i1< os1.getCount(); i1++){
+          float x = (float) os1.getValue(i1);
+          _sinc_weights[i2][i1] = sinc.interpolate(is1,is2,ispike,x,z);
+        }
+      }
+    }
+    
+    private float[] _w;
+    private float[][] _sinc_weights;
+    private float _xs,_zs;
+    private int _ixs,_izs,_nt;
+    private int _n;
+  }
+
+
+
+
+
+
+
+
   public static class ReceiverSource implements Source {
     public ReceiverSource(Receiver receiver) {
       int[][] c = receiver.getIndices();
@@ -87,8 +172,18 @@ public interface Source {
       _nr = receiver.getNr();
       _nt = receiver.getNt();
       _data = receiver.getData();
+      _rec = receiver;
     }
+
     public void add(float[][] ui, int it, int nabsorb) {
+      if(_rec.isSincRec()) {
+        _rec.setDataRadj(ui, it, nabsorb);
+      }else{ 
+        addI(ui, it, nabsorb);
+      }
+    }
+
+    public void addI(float[][] ui, int it, int nabsorb) {
       if (it>=_nt) 
         return;
       for (int ir=0; ir<_nr; ++ir) {
@@ -97,6 +192,7 @@ public interface Source {
         ui[zs][xs] += _data[ir][it];
       }
     }
+    private Receiver _rec;
     private int _nr,_nt;
     private int[] _xr, _zr;
     private float[][] _data;
